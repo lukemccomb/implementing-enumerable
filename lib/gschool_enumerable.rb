d916c19c08a5d9dea32173da4c78158663e55381
Luke McComb <lucasmccomb@gmail.com>
2014-07-31 10:29:13 -0600

    completed exercise

--- lib/gschool_enumerable.rb
+++ lib/gschool_enumerable.rb
@@ -1,3 +1,54 @@
 module GschoolEnumerable
 
+  def g_each
+
+    for i in self
+      yield(i)
+    end
+    return self
+  end
+
+  def g_select
+    ary = []
+    self.g_each do |item|
+      if yield(item)
+        ary << item
+      end
+    end
+    ary
+  end
+
+  def g_reject
+    ary = []
+    self.g_each do |item|
+      ary << item unless yield(item)
+    end
+    ary
+  end
+
+  def g_map
+    ary = []
+    self.g_each do |item|
+      ary << yield(item)
+    end
+    ary
+  end
+
+  def g_find
+    self.g_each do |item|
+      if yield(item)
+       return item
+      end
+    end
+    nil
+  end
+
+  def g_select!
+    self.g_each do |item|
+      unless yield(item)
+        self.delete(item)
+      end
+    end
+  end
+
 end
